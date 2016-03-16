3dc9bdc5c78e3ee950e369e50192e7007bdc04d4
Henry <qoo392@hotmail.com>
2016-03-16 15:46:31 +0800

    add share function

---
+++ app/src/main/java/tw/kits/voicein/activity/QrcodeCreateActivity.java
@@ -0,0 +1,15 @@
+package tw.kits.voicein.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tw.kits.voicein.R;
+
+public class QrcodeCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcode_create);
+    }
+}
