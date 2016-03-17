d668842a7b13601119f904a4bad6f6039e5c6ad8
Henry <qoo392@hotmail.com>
2016-03-17 15:29:08 +0800

    finish search

--- app/src/main/java/tw/kits/voicein/activity/SearchActivity.java
+++ app/src/main/java/tw/kits/voicein/activity/SearchActivity.java
@@ -5,15 +5,22 @@ import android.app.SearchableInfo;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
 import android.widget.LinearLayout;
 
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import tw.kits.voicein.R;
 import tw.kits.voicein.adapter.SearchResultAdapter;
 import tw.kits.voicein.model.Contact;
+import tw.kits.voicein.util.ServiceManager;
+import tw.kits.voicein.util.UserAccessStore;
+import tw.kits.voicein.util.VoiceInService;
 
 public class SearchActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
     private static final String TAG = SearchActivity.class.getSimpleName();
@@ -21,7 +28,7 @@ public class SearchActivity extends AppCompatActivity implements SearchView.OnQu
     List<Contact> mContactList;
     RecyclerView mListView;
     LinearLayout mLayout;
-    SearchResultAdapter mContactAdapter;
+    SearchResultAdapter mSearchAdapter;
     public static String ARG_CONTACTS;
 
     @Override
@@ -29,28 +36,52 @@ public class SearchActivity extends AppCompatActivity implements SearchView.OnQu
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        mContactList = (List<Contact>) getIntent().getSerializableExtra(ARG_CONTACTS);
+        VoiceInService service = ServiceManager.createService(UserAccessStore.getToken());
+        service.getContacts(UserAccessStore.getUserUuid())
+                .enqueue(new Callback<List<Contact>>() {
+                    @Override
+                    public void onResponse(Call<List<Contact>> call, Response<List<Contact>> response) {
+                        if (response.isSuccess()) {
+                            mContactList = response.body();
+                            mSearchAdapter = new SearchResultAdapter(mContactList, SearchActivity.this, mLayout);
+                            mListView.setAdapter(mSearchAdapter);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<Contact>> call, Throwable t) {
+
+                    }
+                });
+
+
         //view
         mSearchView = (SearchView) findViewById(R.id.search_res_sv);
         mListView = (RecyclerView) findViewById(R.id.search_res_lv);
         mLayout = (LinearLayout) findViewById(R.id.search_res_lo_main);
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         SearchableInfo searchableInfo = searchManager.getSearchableInfo(getComponentName());
+        mListView.setLayoutManager(new LinearLayoutManager(this));
         mSearchView.setSearchableInfo(searchableInfo);
         mSearchView.setOnQueryTextListener(this);
-        mContactAdapter = new SearchResultAdapter(mContactList, this, mLayout);
-        mListView.setAdapter(mContactAdapter);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
 
 
     @Override
     public boolean onQueryTextSubmit(String query) {
+
         return false;
+
     }
 
     @Override
     public boolean onQueryTextChange(String newText) {
-        return false;
+        if(mSearchAdapter!=null){
+            mSearchAdapter.getFilter().filter(newText);
+        }
+
+        return true;
     }
 }
