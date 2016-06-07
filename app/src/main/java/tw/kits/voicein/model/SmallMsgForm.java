ca7fee10428327e0516811a8e49a531e33a39443
Henry <qoo392@hotmail.com>
2016-06-07 18:33:47 +0800

    add send msg

---
+++ app/src/main/java/tw/kits/voicein/model/SmallMsgForm.java
@@ -0,0 +1,16 @@
+package tw.kits.voicein.model;
+
+/**
+ * Created by Henry on 2016/6/7.
+ */
+public class SmallMsgForm {
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
